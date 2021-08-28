419b9666bfa34a9cbfa8c045d2e4814f0fcb67ba
prkkhan786 <prkkhan1@gmail.com>
2021-08-29 01:33:26 +0530

    Added Image instead of text

--- src/components/Auth/Welcome.js
+++ src/components/Auth/Welcome.js
@@ -1,33 +1,103 @@
-import React from 'react'
-import {ImageBackground, View, Text,TouchableOpacity,StyleSheet } from 'react-native'
+import React from "react";
+import {
+  ImageBackground,
+  Image,
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 const Welcome = ({ navigation }) => {
-    return(
-      <ImageBackground source={require('./welcome.png')} style={styles.backgroundImage}>
-         <View style={{ justifyContent:'center',alignItems:'center',marginTop:'90%' }}>
-           <View style={{justifyContent:"center",alignItems:"center" }}>
+  const width = Dimensions.get("window").width;
+  return (
+    <ImageBackground
+      source={require("./welcome.png")}
+      style={styles.backgroundImage}
+    >
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "90%",
+        }}
+      >
+        {/* <View style={{justifyContent:"center",alignItems:"center" }}>
            <Text style={{ fontSize:50,fontWeight:'bold',color:"white" }}>somadome</Text>
           <Text style={{ fontSize:15,color:"white" }}>Your journey to the Present</Text>
-           </View>
-  
-          <View style={{marginTop:'60%'}}>
-          <TouchableOpacity style={{ height:50,width:150,backgroundColor:'rgb(37, 150, 190)',marginTop:'10%' }}
-                            onPress={()=>navigation.navigate("Login")}>
-               <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>LOGIN</Text>
-          </TouchableOpacity>  
-          <TouchableOpacity style={{ height:50,width:150,backgroundColor:'rgb(37, 150, 190)',marginTop:'3%' }}
-                            onPress={()=>navigation.navigate("Registration")}>
-               <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>REGISTER</Text>
-          </TouchableOpacity>  
-          </View>
+          </View> */}
+
+        <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            width: width,
+            height: 20,
+          }}
+        >
+          <Image
+            source={require("../../../assets/images/homeicoon.png")}
+            style={{
+              width: 500,
+              height: 70,
+              resizeMode: "contain",
+            }}
+          />
         </View>
-      </ImageBackground>  
-    )
-}
+
+        <View style={{ marginTop: "60%" }}>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: "rgb(37, 150, 190)",
+              marginTop: "10%",
+            }}
+            onPress={() => navigation.navigate("Login")}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: "white",
+                textAlign: "center",
+                paddingTop: 12,
+              }}
+            >
+              LOGIN
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              height: 50,
+              width: 150,
+              backgroundColor: "rgb(37, 150, 190)",
+              marginTop: "3%",
+            }}
+            onPress={() => navigation.navigate("Registration")}
+          >
+            <Text
+              style={{
+                fontSize: 18,
+                color: "white",
+                textAlign: "center",
+                paddingTop: 12,
+              }}
+            >
+              REGISTER
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
+  );
+};
 const styles = StyleSheet.create({
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover', // or 'stretch'
-  }
+    resizeMode: "cover", // or 'stretch',
+  },
 });
 
 export default Welcome;
