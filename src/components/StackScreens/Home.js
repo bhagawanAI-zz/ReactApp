64551cd5994d2c03aae3c714c8ad9f91810a8b61
prkkhan786 <prkkhan1@gmail.com>
2021-08-30 05:57:59 +0530

    changes

--- src/components/StackScreens/Home.js
+++ src/components/StackScreens/Home.js
@@ -1,70 +1,95 @@
-import React from 'react'
-import {Text, TouchableOpacity ,SafeAreaView,View,StyleSheet,Image} from 'react-native'
-import { color } from 'react-native-reanimated';
-
+import React from "react";
+import {
+  Text,
+  TouchableOpacity,
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Image,
+} from "react-native";
+import { color } from "react-native-reanimated";
 
 const Home = ({ navigation }) => {
-    return(
-        <View style={styles.center}>
-      <SafeAreaView style={styles.heading}>
-           <Text adjustsFontSizeToFit numberOfLines={1} style={{ fontSize:30,color:"white"}}>
-                USE
-           </Text></SafeAreaView>
-           <View style={styles.mainContent}>
-           <TouchableOpacity style={styles.roundButton}
-                            onPress={()=>navigation.navigate("Use")}>
-               {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>DOMING AT HOME</Text> */}
-               <Image style={styles.images} source={require("../../../assets/images/ATHOME.png")}></Image>
-          </TouchableOpacity> 
-          <TouchableOpacity style={styles.roundButton}
-                            onPress={()=>navigation.navigate("Dome")}>
-               {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>DOOMING OUT AND ABOUT</Text> */}
-               <Image style={styles.images} source={require("../../../assets/images/DOMINGOUT.png")}></Image>
-          </TouchableOpacity> 
-          <TouchableOpacity style={styles.roundButton}
-                            onPress={()=>navigation.navigate("Use")}>
-               {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>APP ONLY</Text> */}
-               <Image style={styles.images} source={require("../../../assets/images/ATHOME.png")}></Image>
-          </TouchableOpacity> 
-           </View>
-    
-        </View>
-    )
-}
+  return (
+    <View style={styles.center}>
+      <View style={styles.heading}>
+        <Text adjustsFontSizeToFit numberOfLines={1} style={styles.text}>
+          USE
+        </Text>
+      </View>
+      <View style={styles.mainContent}>
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => navigation.navigate("Use")}
+        >
+          {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>DOMING AT HOME</Text> */}
+          <Image
+            style={styles.images}
+            source={require("../../../assets/images/ATHOME.png")}
+          ></Image>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => navigation.navigate("Dome")}
+        >
+          {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>DOOMING OUT AND ABOUT</Text> */}
+          <Image
+            style={styles.images}
+            source={require("../../../assets/images/DOMINGOUT.png")}
+          ></Image>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.roundButton}
+          onPress={() => navigation.navigate("Use")}
+        >
+          {/* <Text style={{ fontSize:18,color:'white',textAlign:'center',paddingTop:12 }}>APP ONLY</Text> */}
+          <Image
+            style={styles.images}
+            source={require("../../../assets/images/ATHOME.png")}
+          ></Image>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    heading:{
-         backgroundColor:"grey",
-         width:'100%',
-        justifyContent:"center",
-        alignItems:"center",
-        flex:0.5
-    },
-    roundButton: {
-     marginTop: 30,
-     width: 190,
-     height: 190,
-     justifyContent: 'center',
-     alignItems: 'center',
-     padding: 10,
-     borderRadius: 100,
-     backgroundColor: '#ccc',
-   },
-   images:{
-        width:190,
-        height:190
-   },
-   center:{
-        justifyContent:"center",
-        alignItems:"center",
-        flex:1
-   },
-   mainContent:{
-        flex:3,
-        justifyContent:"center"
-   }
-   }); 
+  heading: {
+    backgroundColor: "grey",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 0.5,
+  },
+  roundButton: {
+    marginTop: '3%',
+    width: "30%",
+    height: "30%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    borderRadius: 100,
+  },
+  images: {
+    width: 190,
+    height: 190,
+  },
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+  },
+  mainContent: {
+    flex: 3,
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 20,
+    textAlign: "center",
+    textAlignVertical: "center",
+    letterSpacing: 3,
+  },
+});
 
 export default Home;
-
-
--- src/components/StackScreens/Use.js
+++ src/components/StackScreens/Use.js
@@ -1,29 +1,60 @@
-import React from 'react'
-import { SafeAreaView, Text, TouchableOpacity,View } from 'react-native'
-import Feel from '../Common/Feel';
+import React from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import Feel from "../Common/Feel";
 
 const Use = ({ navigation }) => {
-    return(
-        <SafeAreaView style={{flex:1,flexDirection:"column",backgroundColor:"grey"}}>
-             <View style={{flex:2,flexDirection:"row"}}>
-             <Feel navigation={navigation} feeling="Relaxed" imagePath="relaxed"></Feel>
-             <Feel navigation={navigation} feeling="Energized" imagePath="energized"></Feel>
-             </View>
-             <View style={{flex:2,flexDirection:"row"}}>
-             <Feel navigation={navigation} feeling="Recovered" imagePath="recovered"></Feel>
-             <Feel navigation={navigation} feeling="Creative" imagePath="creative"></Feel>
-             </View>
-             <View style={{flex:2,flexDirection:"row",backgroundColor:"blue"}}>
-             <Feel navigation={navigation} feeling="Rested" imagePath="relaxed"></Feel>
-             <Feel navigation={navigation} feeling="Focused" imagePath="energized"></Feel>
-             </View>
-             <View style={{flex:2,flexDirection:"row",backgroundColor:"pink"}}>
-             <Feel navigation={navigation} feeling="Grateful" imagePath="creative"></Feel>
-             <Feel navigation={navigation} feeling="Vital" imagePath="recovered"></Feel>
-             </View>
-        </SafeAreaView>
-        
-    )
-}
+  return (
+    <View style={{ flex: 1, flexDirection: "column" }}>
+      <View style={{ flex: 2, flexDirection: "row" }}>
+        <Feel
+          navigation={navigation}
+          feeling="Relaxed"
+          imagePath="relaxed"
+        ></Feel>
+        <Feel
+          navigation={navigation}
+          feeling="Energized"
+          imagePath="energized"
+        ></Feel>
+      </View>
+      <View style={{ flex: 2, flexDirection: "row" }}>
+        <Feel
+          navigation={navigation}
+          feeling="Recovered"
+          imagePath="recovered"
+        ></Feel>
+        <Feel
+          navigation={navigation}
+          feeling="Creative"
+          imagePath="creative"
+        ></Feel>
+      </View>
+      <View style={{ flex: 2, flexDirection: "row" }}>
+        <Feel
+          navigation={navigation}
+          feeling="Rested"
+          imagePath="relaxed"
+        ></Feel>
+        <Feel
+          navigation={navigation}
+          feeling="Focused"
+          imagePath="energized"
+        ></Feel>
+      </View>
+      <View style={{ flex: 2, flexDirection: "row" }}>
+        <Feel
+          navigation={navigation}
+          feeling="Grateful"
+          imagePath="creative"
+        ></Feel>
+        <Feel
+          navigation={navigation}
+          feeling="Vital"
+          imagePath="recovered"
+        ></Feel>
+      </View>
+    </View>
+  );
+};
 
 export default Use;
