a72c0adafd9b3ee0202b36a5f8c98a3ca0ccb0a4
prkkhan786 <prkkhan1@gmail.com>
2021-10-29 23:44:35 +0530

    bug fix

--- src/components/Common/SessionCount.js
+++ src/components/Common/SessionCount.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet,PixelRatio } from "react-native";
-
+import { View, Text, StyleSheet, PixelRatio } from "react-native";
 
 var FONT_BACK_LABEL = 25;
 var FONT_HEADING = 15;
 if (PixelRatio.get() <= 2) {
-  FONT_BACK_LABEL = 20;
-  FONT_HEADING = 10;
+  FONT_BACK_LABEL = 15;
+  FONT_HEADING = 13;
 }
 
 export default function SessionCount({ text, count, customeStyle }) {
@@ -20,6 +19,6 @@ export default function SessionCount({ text, count, customeStyle }) {
 
 const style = StyleSheet.create({
   flex1: { flex: 1, justifyContent: "center", alignItems: "center" },
-  heading:{color:"white",letterSpacing:0.5},
-  count:{color:"white",paddingTop:20,fontSize:FONT_BACK_LABEL}
+  heading: { color: "white", letterSpacing: 0.5, fontSize: FONT_HEADING },
+  count: { color: "white", paddingTop: 20, fontSize: FONT_BACK_LABEL },
 });
